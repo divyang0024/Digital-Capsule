8f2e1778dbacca7f1c6bd09a6f1cd101fe14b8b1
divayng <divyang0024@example.com>
2024-11-15 14:18:28 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: true,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
 }));
 
 app.options('*', cors());
