393dd914f887fb60973a2df5a442b626aafe258d
divayng <divyang0024@example.com>
2024-11-18 23:27:48 +0530

    private capsule minor bug fix

--- frontend/src/reducer/capsuleReducer.js
+++ frontend/src/reducer/capsuleReducer.js
@@ -48,7 +48,6 @@ export const capsuleReducer = createSlice({
         GET_PRIVATE_CAPSULE_SUCCESS: (state, action) => {
             state.loading = false;
             state.recieved = true,
-                console.log(action.payload);
                 state.capsule = action.payload;
             state.error = null;
         },
